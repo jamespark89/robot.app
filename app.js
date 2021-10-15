ed67d1bc5cef9e963caae259649f97b5841bcba8
JamesPark89 <87226158+JamesPark89@users.noreply.github.com>
2021-10-15 23:55:15 +1100

    add show function to display
    -add grid
    -add show function to display

--- app.js
+++ app.js
@@ -6,33 +6,40 @@ import Robot from "./robot.js";
 let robots = [''];
 let activeRobot ;
 let activeRobotNumber;
+let xycoordinate;
 
 //function for placing a new robot on the table
 function place(x,y,f) {
   robots[robots.length] = new Robot(x,y,f);
-
 //default set the robot1 as activeRobot 
   if(robots.length == '2') {
     activeRobot = robots[1];
     activeRobotNumber = '1';
   }
   console.log(`Placed Robot${robots.length-1}`);
+  show(robots);
 }
 
 function move(){
+  //remove the previos cell
+  unshow();
   activeRobot.move();
+  show(robots);
 }
 
 function left(){
   activeRobot.left();
+  show(robots);
 }
 
 function right(){
   activeRobot.right();
+  show(robots);
 }
 
 //activate robot(number)
 function robot(activeNumber){
+  unshow();
   if(activeNumber > robots.length-1){
     var error = "This number doesn't exit."
     console.log(error);
@@ -40,6 +47,8 @@ function robot(activeNumber){
   }
   activeRobot = robots[activeNumber];
   activeRobotNumber = activeNumber;
+
+  show(robots);
 }
 
 function report(){
@@ -53,13 +62,42 @@ function report(){
   return output;
 }
 
+//show robots in grid
+function show(robots){
+  const coordinate = ['a','b','c','d','e'];
+  let i = 1;
+  while(i<robots.length){
+  const x = robots[i].x;
+  const y = robots[i].y;
+  const xy = coordinate[x-1]+coordinate[y-1];
+  const robotcell = document.getElementById(xy);
+  robotcell.classList.add("placed");
+  i++;}
+  // create active robot make color blue
+  const activex = activeRobot.x;
+  const activey = activeRobot.y;
+  const activexy = coordinate[activex-1]+coordinate[activey-1];
+  const activeRobotCell = document.getElementById(activexy);
+  activeRobotCell.classList.add("active");
+}
+
+function unshow(){
+  const coordinate = ['a','b','c','d','e'];
+  const x = activeRobot.x;
+  const y = activeRobot.y;
+  const xycoordinate = coordinate[x-1]+coordinate[y-1];
+  const robotcell = document.getElementById(xycoordinate);
+
+  robotcell.classList.remove("active");
+  robotcell.classList.remove("placed");
+}
+
 place(1,1,"north");
-report();
-place(2,2,"east");
-report();
+place(2,4,"east");
 place(3,3,"south");
-report();
-robot(3);
-report();
+place(3,1,"south");
 move();
+move();
+robot(4);
+robot(3);
 report();
